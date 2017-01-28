9da99192e4644bca4bed922e5b8d5b1f525fe730
Sianna Puente <siannaapuentec@gmail.com>
2017-01-28 16:36:55 -0500

    spinner dinámico provincia y ciudades

--- Comunitarias/app/src/main/java/com/example/personal/comunitarias/Denuncias/Peticionario.java
+++ Comunitarias/app/src/main/java/com/example/personal/comunitarias/Denuncias/Peticionario.java
@@ -1,9 +1,12 @@
 package com.example.personal.comunitarias.Denuncias;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -13,9 +16,9 @@ import com.example.personal.comunitarias.R;
  * Created by Janina Costa on 28/1/2017.
  */
 
-public class Peticionario extends AppCompatActivity {
+public class Peticionario extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Spinner identidad, tipoIdentificacion, genero, estado_civil, nivelEducacion, nacionalidad, residencia, provincia, ciudad;
-    ArrayAdapter<CharSequence> adapter, adapter2, adapter3, adapter4, adapter5, adapter6, adapter7;
+    ArrayAdapter<CharSequence> adapter, adapter2, adapter3, adapter4, adapter5, adapter6, adapter7,adapter8, adapter9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +75,66 @@ public class Peticionario extends AppCompatActivity {
                 R.array.si_no, android.R.layout.simple_spinner_item);
         adapter7.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         residencia.setAdapter(adapter7);
+
+        //Spinner Provincia
+        provincia = (Spinner) findViewById(R.id.spinner8);
+
+        //Spinner Ciudad
+        ciudad = (Spinner) findViewById(R.id.spinner9);
+
+        loadSpinnerProvincias();
+
+    }
+
+    private void loadSpinnerProvincias() {
+        // Create an ArrayAdapter using the string array and a default spinner
+        // layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.provincias, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        this.provincia.setAdapter(adapter);
+
+        // This activity implements the AdapterView.OnItemSelectedListener
+        this.provincia.setOnItemSelectedListener(this);
+        this.ciudad.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        switch (parent.getId()) {
+            case R.id.spinner8:
+
+                // Retrieves an array
+                TypedArray arrayLocalidades = getResources().obtainTypedArray(
+                        R.array.array_provincia_a_localidades);
+
+                CharSequence[] localidades = arrayLocalidades.getTextArray(pos);
+                arrayLocalidades.recycle();
+
+                // Create an ArrayAdapter using the string array and a default
+                // spinner layout
+                ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(
+                        this, android.R.layout.simple_spinner_item,
+                        android.R.id.text1, localidades);
+
+                // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                // Apply the adapter to the spinner
+                this.ciudad.setAdapter(adapter);
+
+                break;
+
+            case R.id.spinner9:
+
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 
--- Comunitarias/app/src/main/java/com/example/personal/comunitarias/Menu.java
+++ Comunitarias/app/src/main/java/com/example/personal/comunitarias/Menu.java
@@ -53,8 +53,6 @@ public class Menu extends AppCompatActivity implements NavigationView.OnNavigati
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -63,9 +61,6 @@ public class Menu extends AppCompatActivity implements NavigationView.OnNavigati
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-
-
 
         Denuncias.setOnClickListener(new View.OnClickListener() {
             @Override
