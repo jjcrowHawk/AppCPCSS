d2b163468762beacd856022d53f005f07054b75c
juanjcrow <jjcrow@espol.edu.ec>
2017-07-30 01:32:14 -0500

    Libreria agregada para utilizar el explorador de archivos para subir evidencia, modificacion de clase Denuncia para agregar evidencia

--- app/src/main/java/com/example/personal/comunitarias/Denuncias/Denuncia.java
+++ app/src/main/java/com/example/personal/comunitarias/Denuncias/Denuncia.java
@@ -1,5 +1,7 @@
 package com.example.personal.comunitarias.Denuncias;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -14,18 +16,23 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.personal.comunitarias.R;
+import com.ipaulpro.afilechooser.utils.FileUtils;
 
+import java.io.File;
 import java.util.ArrayList;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class Denuncia extends Fragment {
+    private static final int REQUEST_CHOOSER =1234;
     Spinner comparecer, hechos;
     ArrayAdapter<CharSequence> adapter, adapter2;
     private ViewPager viewPager;
     private View view;
     private TabsDenuncia tabs;
-    private Button sgteDenuciado;
-    EditText descripcion;
+    private Button sgteDenuciado,busquedaButton;
+    EditText descripcion,archivoText;
     static String Descripcion_Denuncia;
     static String Comparecer_d;
 
@@ -61,7 +68,9 @@ public class Denuncia extends Fragment {
         comparecer.setAdapter(adapter);
 
         descripcion = (EditText) view.findViewById(R.id.txt_descripcion);
+        archivoText = (EditText) view.findViewById(R.id.archivoText);
         sgteDenuciado = (Button) view.findViewById(R.id.btnDenuncia);
+        busquedaButton = (Button) view.findViewById(R.id.button2);
 
         sgteDenuciado.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,8 +89,41 @@ public class Denuncia extends Fragment {
             }
         });
 
+        busquedaButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent getContentIntent = FileUtils.createGetContentIntent();
 
+                Intent intent = Intent.createChooser(getContentIntent, "Select a file");
+                startActivityForResult(intent, REQUEST_CHOOSER);
+            }
+        });
     }
+     @Override
+     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CHOOSER:
+                if (resultCode == RESULT_OK) {
+
+                    final Uri uri = data.getData();
+
+                    // Get the File path from the Uri
+                    String path = FileUtils.getPath(this.getContext(), uri);
+
+                    // Alternatively, use FileUtils.getFile(Context, Uri)
+                    if (path != null && FileUtils.isLocal(path)) {
+                        File file = new File(path);
+                        Toast.makeText(this.getContext(),"File " + file.toString() +" was selected",Toast.LENGTH_LONG).show();
+                        archivoText.setText(file.toString());
+                    }
+                    else{
+                        System.out.println("Nothing was selected");
+                    }
+                }
+                break;
+        }
+    }
+
     public static String getDescripcion_Denuncia() {
         return Descripcion_Denuncia;
     }
