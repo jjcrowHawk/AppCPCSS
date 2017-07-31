d5a6c12de9d62518ca9c1bcb35bff3196ba7dac8
ejrocafuerte <ejrocafuerte@gmail.com>
2017-07-30 22:10:16 -0500

    control social
    Agrego el activity de transparencia con los fragments para cada seccion con su respectivo contenido proporcionado por la cpccs

--- app/src/main/java/com/example/personal/comunitarias/ControlSocial/DCFragment.java
+++ app/src/main/java/com/example/personal/comunitarias/ControlSocial/DCFragment.java
@@ -1,4 +1,4 @@
-package com.example.personal.comunitarias.AreaSocial;
+package com.example.personal.comunitarias.ControlSocial;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -12,11 +12,11 @@ import com.example.personal.comunitarias.R;
  * Created by erick on 29/7/2017.
  */
 
-public class CPFragment extends Fragment{
+public class DCFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment__pp_cp, container, false);
+        return inflater.inflate(R.layout.fragment__cs_dc, container, false);
     }
 }
--- app/src/main/java/com/example/personal/comunitarias/ControlSocial/VCFragment.java
+++ app/src/main/java/com/example/personal/comunitarias/ControlSocial/VCFragment.java
@@ -1,5 +1,4 @@
-package com.example.personal.comunitarias.AreaSocial;
-
+package com.example.personal.comunitarias.ControlSocial;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,18 +8,15 @@ import android.view.ViewGroup;
 
 import com.example.personal.comunitarias.R;
 
-
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link CCSFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * Created by erick on 29/7/2017.
  */
-public class CCSFragment extends Fragment {
+
+public class VCFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment__pp_ccs, container, false);
+        return inflater.inflate(R.layout.fragment__cs_vc, container, false);
     }
-
 }
